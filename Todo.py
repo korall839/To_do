a34dab7b4e392b130c3bfee232e2b94d3dafbff8
korall839 <Artem.starkov24@yandex.ru>
2026-06-10 22:10:11 +0300

    Add files via upload

--- Todo.py
+++ Todo.py
@@ -3,8 +3,29 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-
+import os
 class MyApp(App):
+    def delite(self,instance):
+        if self.d.text != '':
+            try:
+                txt=''
+                stroca=0
+                with open("do.txt","r") as file:
+                    for line in file:
+                        stroca+=1
+                        if int(self.d.text) != stroca:
+                            txt=txt+line
+                with open("do.txt","w") as file:
+                    file.write(txt)
+                with open("do.txt","r") as file:
+                    txt=' '+str(file.readlines())
+                    self.b.text=txt.replace(',','''
+        ''').replace('[','').replace(']','').replace('\'','').replace('\\n','')
+            except FileNotFoundError:
+                pass
+            if self.b.text == ' ':
+                os.remove('do.txt')
+                self.b.text='создайте задачу!'
     def do(self,instance):
         with open("do.txt","a") as file:
             file.write('\n'+self.create_do.text)
@@ -29,6 +50,16 @@ class MyApp(App):
             pos_hint=({'x':0.45,'y':0.6}),
             size_hint=(0.1,0.1)
         )
+        self.c=Button(
+            text="удалить задачу",
+            pos_hint=({'x':0.2,'y':0.05}),
+            size_hint=(0.1,0.1)
+        )
+        self.d=TextInput(
+            text="",
+            pos_hint=({"x":0.2,"y":0.15}),
+            size_hint=(0.1,0.1)
+        )
         try:
             with open("do.txt","r") as file:
                 txt=' '+str(file.readlines())
@@ -36,10 +67,14 @@ class MyApp(App):
     ''').replace('[','').replace(']','').replace('\'','').replace('\\n','')
         except FileNotFoundError:
             pass
+        self.c.bind(on_press=self.delite)
         self.a.bind(on_press=self.do)
         layout.add_widget(self.create_do)
         layout.add_widget(self.a)
         layout.add_widget(self.b)
+        layout.add_widget(self.c)
+        layout.add_widget(self.d)
+        print(self.b.text+"|")
         return layout
 
 if __name__ == '__main__':
